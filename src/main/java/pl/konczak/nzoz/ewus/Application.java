4bab8da3dbb09a64b9b729ef5a0e92824024f516
Piotr Konczak <konczak.piotrek@gmail.com>
2017-01-29 19:33:02 +0100

    something has generated

---
+++ src/main/java/pl/konczak/nzoz/ewus/Application.java
@@ -0,0 +1,13 @@
+package pl.konczak.nzoz.ewus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
