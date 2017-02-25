5b51de1722cca2b321da055be292fea1c3a174c1
Piotr Konczak <konczak.piotrek@gmail.com>
2017-02-25 20:12:14 +0100

    configure job checking CWU for all pesel every first, second, penultimate and last day of the month

--- src/main/java/pl/konczak/nzoz/ewus/Application.java
+++ src/main/java/pl/konczak/nzoz/ewus/Application.java
@@ -3,7 +3,9 @@ package pl.konczak.nzoz.ewus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableCaching
 @SpringBootApplication
 public class Application {
---
+++ src/main/java/pl/konczak/nzoz/ewus/job/CheckCWUForAllJob.java
@@ -0,0 +1,51 @@
+package pl.konczak.nzoz.ewus.job;
+
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import pl.konczak.nzoz.ewus.client.ewus.checkcwu.CheckCWUStatusFacade;
+
+@Component
+public class CheckCWUForAllJob {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CheckCWUForAllJob.class);
+
+    private final CheckCWUStatusFacade checkCWUStatusFacade;
+
+    public CheckCWUForAllJob(CheckCWUStatusFacade checkCWUStatusFacade) {
+        this.checkCWUStatusFacade = checkCWUStatusFacade;
+    }
+
+    @Scheduled(cron = "${job.checkCWUForAll.cron}")
+    public void run() {
+        LocalDate now = LocalDate.now();
+        if (!isOneOfTwoFirstDaysOfTheMonth(now)
+                && !isOneOfTwoEndDaysOfTheMonth(now)) {
+            return;
+        }
+        LOGGER.info("CheckCWUForAllJob starts");
+
+        try {
+            checkCWUStatusFacade.checkCWUForAll();
+        } catch (Exception ex) {
+            LOGGER.error("checkCWUForAll failed", ex);
+        }
+
+        LOGGER.info("CheckCWUForAllJob ends");
+    }
+
+    private boolean isOneOfTwoEndDaysOfTheMonth(LocalDate now) {
+        int monthLength = now.getMonth().length(now.isLeapYear());
+        return now.getDayOfMonth() == monthLength
+                || now.getDayOfMonth() == (monthLength - 1);
+    }
+
+    private boolean isOneOfTwoFirstDaysOfTheMonth(LocalDate now) {
+        return now.getDayOfMonth() == 1
+                || now.getDayOfMonth() == 2;
+    }
+}
